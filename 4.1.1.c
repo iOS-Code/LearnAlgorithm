6de5b3a3042b3d93f3f60e607e157cc02f402ec8
岳琛 <yuechen@100tal.com>
2018-03-09 16:23:47 +0800

    no message

--- 4.1.1.c
+++ 4.1.1.c
@@ -37,3 +37,22 @@ int main()
     getchar();getchar();
     return 0;
 }
+
+
+/**
+ 深度优先搜索的关键在于解决“当下该如何做”
+ 
+ 
+ void dfs (int step)
+ {
+    判断边界
+    尝试每一种可能  for (i=1;i<=n;i++)
+    {
+        继续下一步 dfs(step+1)
+    }
+    返回
+ }
+ 
+ 
+ 
+ */
